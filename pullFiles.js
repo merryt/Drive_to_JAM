99a26c9933ea725d3e0f3e8596c5cbdac0d6db3f
merryt <tymerry@gmail.com>
2022-01-28 08:35:49 -0600

    hopes and dreams

--- pullFiles.js
+++ pullFiles.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const config = require('config.json')('./config.json');
 const nodePandoc = require('node-pandoc');
 
@@ -20,7 +20,7 @@ fs.readFile('credentials.json', (err, content) => {
 });
 
 function authorize(credentials, callback) {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
+    const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(
         client_id, client_secret, redirect_uris[0]);
 
@@ -59,8 +59,8 @@ function getAccessToken(oAuth2Client, callback) {
 
 
 function listFiles(auth) {
-    return new Promise(function(resolve, reject){
-        const drive = google.drive({version: 'v3', auth});
+    return new Promise(function (resolve, reject) {
+        const drive = google.drive({ version: 'v3', auth });
         return drive.files.list({
             pageSize: 50,
             q: `'${config.publishingFolderID}' in parents`,
@@ -78,7 +78,7 @@ function listFiles(auth) {
 }
 
 
-function writeStream(slugifiedFileName){
+function writeStream(slugifiedFileName) {
     const filePath = slugifiedFileName;
     return fs.createWriteStream(filePath, {
         encoding: 'utf-8'
@@ -86,7 +86,7 @@ function writeStream(slugifiedFileName){
 }
 
 
-function moveFile(fileId, targetDirectory, drive){
+function moveFile(fileId, targetDirectory, drive) {
     /// moves a file from its current location to a given directory.
     /// taskes file id, the directory you are moving it to, and an authenticated drive instance
 
@@ -118,53 +118,54 @@ function moveFile(fileId, targetDirectory, drive){
 }
 
 
-function grabFiles(auth){
-    const drive = google.drive({version: 'v3', auth});
-    listFiles(auth).then((files) =>{
+function grabFiles(auth) {
+    const drive = google.drive({ version: 'v3', auth });
+    listFiles(auth).then((files) => {
         let promises = []
-        files.forEach((file, index) =>{
-            let promise = new Promise((resolve, reject)=>{
+        files.forEach((file, index) => {
+            let promise = new Promise((resolve, reject) => {
                 const slugifiedName = slugify(file.name)
                 const slugifiedFileName = `docx/${slugifiedName}.docx`
                 file.slugifiedFileName = slugifiedFileName
                 drive.files.export({
                     fileId: file.id,
                     mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                },{responseType: 'stream'}, function(err, res){
+                }, { responseType: 'stream' }, function (err, res) {
                     res.data
-                       .on('end', () => {
-                           resolve("done")
-                           console.log(`Finished writing the docx for ${file.name}`);
-                       })
-                       .on('error', err => {
-                           reject(err)
-                           console.log('Error', err);
-                       }).pipe(writeStream(slugifiedFileName));
+                        .on('end', () => {
+                            resolve("done")
+                            console.log(`Finished writing the docx for ${file.name}`);
+                        })
+                        .on('error', err => {
+                            reject(err)
+                            console.log('Error', err);
+                        }).pipe(writeStream(slugifiedFileName));
 
                 });
             })
             promises.push(promise)
         });
         Promise.all(promises)
-               .then(() => {
-                   convertToMarkdown(files)
-                   files.forEach((file,index)=>{
-                       moveFile(file.id, config.publishedFolderID, drive)
-                   })
-               })
-               .catch((e) => console.error('crap, not everything finished downloading or something... idk'));
+            .then(() => {
+                convertToMarkdown(files)
+                files.forEach((file, index) => {
+                    moveFile(file.id, config.publishedFolderID, drive)
+                })
+            })
+            .catch((e) => console.error('crap, not everything finished downloading or something... idk'));
     })
 }
 
 
-function convertToMarkdown(files){
-    files.forEach((file)=>{
+function convertToMarkdown(files) {
+    files.forEach((file) => {
+
         let src = file.slugifiedFileName;
         const slugifiedName = slugify(file.name)
         const markdownFileName = `./markdown/${slugifiedName}.md`
-        args = ['-f','docx','-t','markdown','-o', markdownFileName, '--wrap=none'];
-        const callback = (err, result)=> {
-            if (err) console.error('Oh Nos: ',err)
+        args = ['-f', 'docx', '-t', 'markdown', '-o', markdownFileName, '--wrap=none'];
+        const callback = (err, result) => {
+            if (err) console.error('Oh Nos: ', err)
             secondpassMDEdit(markdownFileName)
             return result
         }
@@ -175,53 +176,59 @@ function convertToMarkdown(files){
 
 }
 
-function secondpassMDEdit(markdownFileName){
-    fs.readFile(markdownFileName, 'utf8', function (err,data) {
-        if (err) {return console.log(err);}
+function secondpassMDEdit(markdownFileName) {
+    fs.readFile(markdownFileName, 'utf8', function (err, data) {
+        if (err) { return console.log(err); }
+
 
         var result = data.replace(/\\-\\--/g, '---');
 
         var whatToSelect = /(?<!\[).*(?!\]\{dir="ltr")/gim
         var whatToKeep = /(?<=\[).*(?=\]\{dir="ltr")/gim
 
-        result = result.replace(whatToSelect, function(str){
-            if(str.match(whatToKeep) == null){
-                return ""
-            }
-            return str.match(whatToKeep)
-        })
+        // I have no clue the issues that were happening with LTR text getting wonkey
+        // but I am pulling this out for now, because I am to lazy to fix it.
+        if (false) {
+            result = result.replace(whatToSelect, function (str) {
+                if (str.match(whatToKeep) == null) {
+                    return ""
+                }
+                return str.match(whatToKeep)
+            })
+        }
 
         indexOfEndOfFrontmatter = result.indexOf('---', + 1);
         frontmatter = cleanFrontMatter(result.slice(0, indexOfEndOfFrontmatter + 3));
         markdownContent = cleanMarkdownContent(result.slice(indexOfEndOfFrontmatter + 3, -1));
 
-        mdData = frontmatter + markdownContent
+        mdData = frontmatter + markdownContent;
+
         fs.writeFile(markdownFileName, mdData, 'utf8', function (err) {
             if (err) return console.log(err);
         });
     });
 }
-function cleanFrontMatter(frontmatter){
+function cleanFrontMatter(frontmatter) {
     frontmatter = frontmatter.replace(/\\/g, "")
     return frontmatter
 }
 
-function cleanMarkdownContent(mdContant){
-    mdContant = mdContant.replace(/\*\*(.+?)\*\*/g,function(str) { return "**" + str.slice(2,-2).trim() + "**"; })
-    mdContant = mdContant.replace(/(^|\n)\-(.\n?)*/g,function(str) { return str.replace(/^\s*>/gm,"")});
-    mdContant = mdContant.replace(/(^|\n)\d.(.\n?)*/g,function(str) { return str.replace(/^\s*>/gm,"")});
-    mdContant = mdContant.replace(/{\.underline}/g,"");
-    mdContant = mdContant.replace(/\[\[/g,"[");
-    mdContant = mdContant.replace(/\]\]/g,"]");
+function cleanMarkdownContent(mdContant) {
+    mdContant = mdContant.replace(/\*\*(.+?)\*\*/g, function (str) { return "**" + str.slice(2, -2).trim() + "**"; })
+    mdContant = mdContant.replace(/(^|\n)\-(.\n?)*/g, function (str) { return str.replace(/^\s*>/gm, "") });
+    mdContant = mdContant.replace(/(^|\n)\d.(.\n?)*/g, function (str) { return str.replace(/^\s*>/gm, "") });
+    mdContant = mdContant.replace(/{\.underline}/g, "");
+    mdContant = mdContant.replace(/\[\[/g, "[");
+    mdContant = mdContant.replace(/\]\]/g, "]");
     return mdContant
 }
 
 
-function slugify(text){
+function slugify(text) {
     return text.toString().toLowerCase()
-               .replace(/\s+/g, '-')           // Replace spaces with -
-               .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-               .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-               .replace(/^-+/, '')             // Trim - from start of text
-               .replace(/-+$/, '');            // Trim - from end of text
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
 }
